c7a04e76942d2e8c2f9adb0ee2833e4d0c1484d0
seanrogan <srogan88@gmail.com>
2022-06-02 15:26:10 -0400

    changes to webscraper

--- src/main/java/com/seanrogandev/weatherscraper/app/webscraper/Webscraper.java
+++ src/main/java/com/seanrogandev/weatherscraper/app/webscraper/Webscraper.java
@@ -11,72 +11,64 @@ import java.util.*;
 
 public class Webscraper {
     final public String baseUrl = "https://www.mountain-forecast.com";
-    String weatherConditionsRow = "forecast__table-summary";
-    String maxTempRow = "forecast__table-max-temperature";
-    String minTempRow = "forecast__table-min-temperature";
-    String windChillRow = "forecast__table-chill";
-    String rainFallRow = "forecast__table-rain";
-    String snowFallRow = "forecast__table-snow";
-    String windRow = "forecast__table-wind";
 
-    public WeatherReport getWeatherData(String url) throws IOException {
 
-        WeatherReport report = new WeatherReport();
+    public List<String[]> getWeatherData(String url) throws IOException {
+        List<String[]> dataList = new ArrayList<>();
+        String weatherConditionsRow = "forecast__table-summary";
+        String maxTempRow = "forecast__table-max-temperature";
+        String minTempRow = "forecast__table-min-temperature";
+        String windChillRow = "forecast__table-chill";
+        String rainFallRow = "forecast__table-rain";
+        String snowFallRow = "forecast__table-snow";
+        String windRow = "forecast__table-wind";
         Document doc = Jsoup.connect(url).get();
+
 
         //get high temps
         Elements maxTempElements = doc.getElementsByClass(maxTempRow)
                 .select("span.temp");
-        String[] maxTemps = collectToArray(maxTempElements.iterator());
+        dataList.add(collectToArray(maxTempElements.iterator()));
 
 
         //get low temps
         Elements minTempElements = doc.getElementsByClass(minTempRow)
                 .select("span.temp");
-        String[] minTemps = collectToArray(minTempElements.iterator());
+        dataList.add(collectToArray(minTempElements.iterator()));
 
 
         //get windchill temps
         Elements windChillElements = doc.getElementsByClass(windChillRow)
                 .select("span.temp");
-        String[] windChillTemps = collectToArray(windChillElements.iterator());
+        dataList.add(collectToArray(windChillElements.iterator()));
 
 
         //get snowfall
         Elements snowFallElements = doc.getElementsByClass(snowFallRow)
                 .select("td.forecast__table-relative")
                 .select("span.snow");
-        String[] snowFall = collectToArray(snowFallElements.iterator());
+        dataList.add(collectToArray(snowFallElements.iterator()));
 
 
         //get rainfall
         Elements rainFallElements = doc.getElementsByClass(rainFallRow)
                 .select("td.forecast__table-relative")
                 .select("span.rain");
-        String[] rainFall = collectToArray(rainFallElements.iterator());
+        dataList.add(collectToArray(rainFallElements.iterator()));
 
 
         //get weather elements
         Elements weatherConditionElements = doc.getElementsByClass(weatherConditionsRow)
                 .select("td");
-        String[] weatherConditions = collectToArray(weatherConditionElements.iterator());
+        dataList.add(collectToArray(weatherConditionElements.iterator()));
 
 
         //get wind elements
         Elements windElements = doc.getElementsByClass(windRow)
                 .select("tr.forecast__table-wind");
-        String[] windStats = getWindConditions(windElements.select("td.iconcell").iterator());
+        dataList.add(getWindConditions(windElements.select("td.iconcell").iterator()));
 
-
-        System.out.println(Arrays.toString(windStats));
-        System.out.println(Arrays.toString(weatherConditions));
-        System.out.println(Arrays.toString(maxTemps));
-        System.out.println(Arrays.toString(minTemps));
-        System.out.println(Arrays.toString(windChillTemps));
-        System.out.println(Arrays.toString(rainFall));
-        System.out.println(Arrays.toString(snowFall));
-
-        return report;
+        return dataList;
     }
 
     private String[] collectToArray(Iterator<Element> itr) {
