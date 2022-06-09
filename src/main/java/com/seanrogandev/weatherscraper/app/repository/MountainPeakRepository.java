7af53de8b7c0e89741dc381c88353d6b4ddc55ba
seanrogan <srogan88@gmail.com>
2022-06-09 16:55:10 -0400

    adding interface methods

--- src/main/java/com/seanrogandev/weatherscraper/app/repository/MountainPeakRepository.java
+++ src/main/java/com/seanrogandev/weatherscraper/app/repository/MountainPeakRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface MountainPeakRepository extends JpaRepository<MountainPeak, Long> {
-
+    @Query("SELECT m FROM mountain_peak m WHERE m.peakId = ?1 AND m.rangeId = ?2" )
+    MountainPeak getPeakByPeakAndRangeId(Long peakId, Long rangeId);
 }
--- src/main/java/com/seanrogandev/weatherscraper/app/repository/WeatherReportRepository.java
+++ src/main/java/com/seanrogandev/weatherscraper/app/repository/WeatherReportRepository.java
@@ -3,7 +3,13 @@ package com.seanrogandev.weatherscraper.app.repository;
 import com.seanrogandev.weatherscraper.app.entities.MountainRange;
 import com.seanrogandev.weatherscraper.app.entities.WeatherReport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface WeatherReportRepository extends JpaRepository<WeatherReport, Long> {
+
+    @Query("SELECT report FROM weather_report report WHERE peakId = ?1 AND ")
+    List<WeatherReport> getWeatherReportByPeakId(Long peakId);
 
     }
